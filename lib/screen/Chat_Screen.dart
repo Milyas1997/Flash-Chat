066a97502c3528fb1e9f039df0e0be59f456ac3b
Muhammad Ilyas <ilyasmaya1997@gmail.com>
2023-02-26 08:35:50 +0000

    issues solved

--- lib/screen/Chat_Screen.dart
+++ lib/screen/Chat_Screen.dart
@@ -15,8 +15,9 @@ class _ChatScreenState extends State<ChatScreen> {
   final Currentuser = FirebaseAuth.instance.currentUser;
   late String message;
   //late String currenU;
+  var fieldE = TextEditingController();
 
-  Future<void> getMessage() async {
+  getMessage() async {
     await for (var snapshot
         in FirebaseFirestore.instance.collection('messages').snapshots()) {
       for (var snapshot in snapshot.docs) {
@@ -28,6 +29,7 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -55,6 +57,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 color: Colors.white,
               ),
               onPressed: () async {
+                // getMessage();
                 //             await for (var snapshot
                 //     in FirebaseFirestore.instance.collection('messages').snapshots()) {
                 //   for (var snapshot in snapshot.docs) {
@@ -75,30 +78,75 @@ class _ChatScreenState extends State<ChatScreen> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(10.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Text(getMessage().toString()),
               StreamBuilder<QuerySnapshot<Object>>(
                 stream: FirebaseFirestore.instance
                     .collection('messages')
                     .snapshots(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.hasData) {
-                    final messages = snapshot.data.document;
-                    List<Text> messagelist = [];
+                    final message = snapshot.data.docs;
+                    List<Widget> messagelist = [];
 
-                    for (var message in messages) {
-                      final txtmessage = message.data['text'];
-                      final sendermail = message.data['sender'];
-                      final mwidget = Text('$txtmessage from $sendermail');
-                      print('$txtmessage from $sendermail');
+                    //print(messages);
+                    bool isequal;
+                    for (var messages in message) {
+                      print(messages);
+                      final txtmessage = messages['text'];
+                      final sendermail = messages['sender'];
+                      if (FirebaseAuth.instance.currentUser == sendermail) {
+                        isequal = true;
+                      } else {
+                        isequal = false;
+                      }
+                      final mwidget = Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              sendermail,
+                              style:
+                                  TextStyle(fontSize: 10, color: Colors.white),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: isequal == true
+                                    ? Colors.green
+                                    : Colors.blue,
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Text(
+                                txtmessage,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Divider(
+                              color: Colors.white,
+                              thickness: 0.2,
+                            )
+                          ],
+                        ),
+                      );
+                      // print('$txtmessage from $sendermail');
                       messagelist.add(mwidget);
+                      
                     }
 
                     return Expanded(
-                      child: Column(
+                      child: ListView(
+                        padding: EdgeInsets.all(0),
                         children: messagelist,
                       ),
                     );
@@ -115,41 +163,54 @@ class _ChatScreenState extends State<ChatScreen> {
                   }
                 },
               ),
-              Row(
-                children: [
-                  Expanded(
-                    flex: 5,
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        fillColor: Colors.white38,
-                        border: InputBorder.none,
-                        hintText: 'Type message here...',
-                        hintStyle: TextStyle(color: Colors.grey),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 0.0),
+                        child: TextFormField(
+                          controller: fieldE,
+                          decoration: const InputDecoration(
+                            fillColor: Colors.white,
+                            border: InputBorder.none,
+                            hintText: 'Type message here...',
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          onChanged: (value) {
+                            message = value;
+                          },
+                        ),
                       ),
-                      onChanged: (value) {
-                        message = value;
-                      },
                     ),
-                  ),
-                  Expanded(
-                      flex: 1,
-                      child: GestureDetector(
-                        onTap: () {
-                          FirebaseFirestore.instance
-                              .collection('messages')
-                              .add({
-                            'text': message,
-                            'sender': FirebaseAuth.instance.currentUser!.email
-                                .toString()
-                          });
-                        },
-                        child: const Text('Send',
-                            style: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold)),
-                      ))
-                ],
+                    Expanded(
+                        flex: 1,
+                        child: GestureDetector(
+                          onTap: () {
+                            FirebaseFirestore.instance
+                                .collection('messages')
+                                .add({
+                              'text': message,
+                              'sender': FirebaseAuth.instance.currentUser!.email
+                                  .toString()
+                            });
+                            fieldE.clear();
+                          },
+                          child: const Text('Send',
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold)),
+                        ))
+                  ],
+                ),
               )
             ],
           ),
